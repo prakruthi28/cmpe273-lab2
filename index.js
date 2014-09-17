152f7dac1cb8e1c3334905c65860bdede7867abf
Prakruthi Nagaraj <prakruthi28@gmail.com>
2014-09-16 23:22:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -53,8 +53,19 @@ function del(request, response) {
  	// TODO: remove session id via login.logout(xxx)
  	// No need to set session id in the response cookies since you just logged out!
 	var cookies = request.cookies;
-	var sid = cookies['session_id'];
-	login.logout(sid);
+	console.log(cookies);
+	if ('session_id' in cookies) {
+		var sid = cookies['session_id'];
+		if ( login.isLoggedIn(sid) ) {
+			login.logout(sid);
+         	response.end('Logged out from the server\n');
+		} else {
+			response.end("Invalid session_id! Please login again\n");
+		}
+	} else {
+		response.end("Please login via HTTP POST\n");
+	}
+
 
 };
 
