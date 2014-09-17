aa95743466209c41c57d6777a40df179905adf66
Prakruthi Nagaraj <prakruthi28@gmail.com>
2014-09-16 22:35:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,25 +36,59 @@ function get(request, response) {
 };
 
 function post(request, response) {
+	var name = request.body.name;
+	var email = request.body.email;
+	var newSessionId = login.login(name, email);
+	response.setHeader('Set-Cookie','session_id='+newSessionId);
 	// TODO: read 'name and email from the request.body'
 	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
 	// TODO: set new session id to the 'session_id' cookie in the response
 	// replace "Logged In" response with response.end(login.hello(newSessionId));
-
-	response.end("Logged In\n");
+	response.end(login.hello(newSessionId));
+	//response.end("Logged In\n");
 };
 
 function del(request, response) {
 	console.log("DELETE:: Logout from the server");
  	// TODO: remove session id via login.logout(xxx)
  	// No need to set session id in the response cookies since you just logged out!
+	var cookies = request.cookies;
+	console.log(cookies);
+	if ('session_id' in cookies) {
+		var sid = cookies['session_id'];
+		if ( login.isLoggedIn(sid) ) {
+			login.logout(sid);
+         	response.end('Logged out from the server\n');
+		} else {
+			response.end("Invalid session_id! Please login again\n");
+		}
+	} else {
+		response.end("Please login via HTTP POST\n");
+	}
 
-  	response.end('Logged out from the server\n');
 };
 
 function put(request, response) {
 	console.log("PUT:: Re-generate new seesion_id for the same user");
 	// TODO: refresh session id; similar to the post() function
+	var cookies = request.cookies;
+	console.log(cookies);
+	if ('session_id' in cookies) {
+		var sid = cookies['session_id'];
+		if ( login.isLoggedIn(sid) ) {
+			var name = 'test';
+			var email = 'a@a.com';
+			login.logout(sid);
+			sid = login.login(name, email);
+			response.setHeader('Set-Cookie','session_id='+sid);
+			response.end(login.hello(sid));
+		} else {
+			response.end("Invalid session_id! Please login again\n");
+		}
+
+	} else {
+		response.end("Please login via HTTP POST\n");
+	}
 
 	response.end("Re-freshed session id\n");
 };
